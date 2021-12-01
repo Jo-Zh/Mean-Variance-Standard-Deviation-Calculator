09d7bc7e343635da13688209a84e725e2279d29d
Joannaweina <93627519+JoannaWeinaZhong@users.noreply.github.com>
2021-12-01 23:44:51 +0000

    Initial commit

---
+++ Calculator.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def calculate(list):
+    list=np.array(list)
+    
+    if len(list)<9:
+        raise ValueError("List must contain nine numbers.")
+    else:
+        myArray=list.reshape((3,3))
+        output_mean_1=np.mean(myArray, axis=1)
+        output_mean_0=np.mean(myArray, axis=0)
+        output_mean=np.mean(myArray)
+        output_var_1=np.var(myArray, axis=1)
+        output_var_0=np.var(myArray, axis=0)
+        output_var=np.var(myArray)
+        output_std_1=np.std(myArray, axis=1)
+        output_std_0=np.std(myArray, axis=0)
+        output_std=np.std(myArray)
+        output_max_1=np.max(myArray, axis=1)
+        output_max_0=np.max(myArray, axis=0)
+        output_max=np.max(myArray)
+        output_min_1=np.min(myArray, axis=1)
+        output_min_0=np.min(myArray, axis=0)
+        output_min=np.min(myArray)
+        output_sum_1=np.sum(myArray, axis=1)
+        output_sum_0=np.sum(myArray, axis=0)
+        output_sum=np.sum(myArray)
+        result={
+        'mean': [output_mean_0.tolist(), output_mean_1.tolist(), output_mean.tolist()],
+        'variance':[output_var_0.tolist(), output_var_1.tolist(), output_var.tolist()],
+        'standard deviation':[output_std_0.tolist(), output_std_1.tolist(), output_std.tolist()],
+        'max':[output_max_0.tolist(), output_max_1.tolist(), output_max.tolist()],
+        'min':[output_min_0.tolist(), output_min_1.tolist(), output_min.tolist()],
+        'sum':[output_sum_0.tolist(), output_sum_1.tolist(), output_sum.tolist()],
+        }
+        return result
+    
---
+++ main.py
@@ -0,0 +1,8 @@
+import Calculator
+#user input
+list1=[]
+for i in range(0,9):
+  element=float(input('enter a number'))
+  list1.append(element)
+#output result
+print(Calculator.calculate(list1))
